57ee8c6c52422848e652c0b39bebfde89eebac89
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-27 15:52:21 +0530

    add.vehicle class Remove.ParkingLot class

---
+++ src/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+    public int spotNeed;
+    public VehicleSize size;
+
+    public VehicleSize getSize() {
+        return size;
+    }
+    public int getSpotNeed() {
+        return spotNeed;
+    }
+}
