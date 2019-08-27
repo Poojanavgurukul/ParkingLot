a0939aeb6a6d28c09f1e6e26e5998b89b3b624d0
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-27 16:14:27 +0530

    remove.VehicleSize object

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,5 @@
 public class Car extends Vehicle {
     public Car(){
         spotNeed=2;
-        size=VehicleSize.CarSize;
     }
 }
--- src/VehicleSize.java
+++
@@ -1,3 +0,0 @@
-public enum  VehicleSize {
-    BikeSize,CarSize,BusSize
-}
