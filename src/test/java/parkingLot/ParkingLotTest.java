3d760ca60011609a424256db56f3320f23b73ab5
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-28 12:03:23 +0530

    add.Test case for unPark method

--- src/test/java/parkingLot/ParkingLotTest.java
+++ src/test/java/parkingLot/ParkingLotTest.java
@@ -7,10 +7,22 @@ import static org.junit.Assert.assertEquals;
 public class ParkingLotTest {
     @Test
     public void vehicleShouldPark() {
-        Car car=new Car("xyz12");
+        Car car = new Car("xyz12");
         String expected = "Vehicle can park";
-        ParkingLot parkingLot=new ParkingLot();
-        String actual=parkingLot.park(car);
-        assertEquals(expected,actual);
+        ParkingLot parkingLot = new ParkingLot();
+        String actual = parkingLot.park(car);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void vehicleShouldUnPark() {
+        Car car = new Car("xyz12");
+        Bike bike = new Bike("xas14");
+        String expected = "xas14 Vehicle unPark";
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.park(car);
+        parkingLot.park(bike);
+        String actual = parkingLot.unPark(parkingLot.getVehicles(), bike);
+        assertEquals(expected, actual);
     }
 }
