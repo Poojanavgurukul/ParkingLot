b277bc3f6ba7f18dce4a43e799667078303fa25e
Poojanavgurukul <Pooja17@navgurukul.org>
2019-08-29 11:28:49 +0530

    initial commit

--- src/main/java/parkingLot/ParkingLotApp.java
+++ src/main/java/parkingLot/ParkingLotApp.java
@@ -7,12 +7,24 @@ public class ParkingLotApp {
         ParkingLot parkingLot = new ParkingLot(20);
 
         Bike bike1 = new Bike("1");
-        parkingLot.park(bike1);
+        System.out.println(parkingLot.park(bike1));
 
         Bus bus = new Bus("2");
-        parkingLot.park(bus);
+        System.out.println(parkingLot.park(bus));
+
+        Bus bus1 = new Bus("4");
+        System.out.println(parkingLot.park(bus1));
+
+        Bus bus2 = new Bus("5");
+        System.out.println(parkingLot.park(bus2));
+
+        Bus bus3 = new Bus("6");
+        System.out.println(parkingLot.park(bus3));
 
         Bike bike2 = new Bike("1");
-        parkingLot.park(bike2);
+        System.out.println(parkingLot.park(bike2));
+
+        Bus bus4 = new Bus("6");
+        System.out.println(parkingLot.park(bus4));
     }
 }
